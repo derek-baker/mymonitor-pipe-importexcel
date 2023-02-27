15cbb5dac5f456ac928487a2600db587dbe5e51e
derek-baker <dbaker@sdgnys.com>
2023-02-26 19:53:00 -0500

    Move some code to functions in module

--- CaptureData.ps1
+++ CaptureData.ps1
@@ -16,7 +16,7 @@ Assert-OperatingSystem
 $data = Get-WindowTime -Minutes $sessionLengthInMinutes
 $outputLocation = "$outputLogsDirectory\AppUsageData$(Get-Timestamp).json"
 
-#Create file using New-Item to ensure the parent directory is also created.
+# Create file using New-Item to ensure any parent directories are also created.
 New-Item -Path $outputLocation -ItemType File -Force | Out-Null
 
 $data | ConvertTo-Json | Out-File -FilePath $outputLocation -Force
--- GenerateReport.ps1
+++ GenerateReport.ps1
@@ -1,4 +1,3 @@
-# TODO: Support alternative 
 param(
     [Parameter(Mandatory=$false)]
     $inputDataDirectory = "$PSScriptRoot\Logs",
@@ -13,43 +12,28 @@ Import-Module './Functions/GenerateReport.Functions.psm1' -Force
 Install-Dependency -moduleName 'ImportExcel' -moduleVersion 7.8.4 # https://github.com/dfinke/ImportExcel
 Assert-OperatingSystem
 
-$dataFilePaths = Get-ChildItem $inputDataDirectory -Recurse `
-    | Where-Object { $_.Extension -eq '.json' } `
-    | ForEach-Object { $_.FullName }
-
-$logs = $dataFilePaths | ForEach-Object {
-    $rawJson = Get-Content $_ -Raw
-    ConvertFrom-Json -InputObject $rawJson
-}
-
-$logEntries = @()
-foreach ($log in $logs) {
-    foreach ($entry in $log) {
-        # TODO: Do this so it isn't slow
-        $logEntries += [PSCustomObject]@{Application = $entry.Application; Seconds = $entry.Time.TotalSeconds}
-    }
-}
-
+$dataFilePaths = Get-InputDataFilenames -inputDataDirectory $inputDataDirectory
+$logs = Read-Inputfiles -dataFilePaths $dataFilePaths 
+$logEntries = Parse-InputFiles -logs $logs
 $distinctApps = $logEntries | Select-Object -ExpandProperty Application -Unique
-$summaryData = @()
 
-$distinctApps | ForEach-Object {
+$summaryData = $distinctApps | ForEach-Object {
     $app = $_
-    $totalSeconds = 0
 
+    $totalSeconds = 0
     $logEntries `
         | Where-Object { $_.Application -eq $app } `
-        | ForEach-Object { $totalSeconds += $_.Seconds } # TODO: Do this so it isn't slow
+        | ForEach-Object { $totalSeconds += $_.Seconds } 
 
-    $summaryData += [PSCustomObject]@{Application = $app; Minutes = $totalSeconds / 60}
+    [PSCustomObject]@{Application = $app; Minutes = $totalSeconds / 60}
 }
 
 $reportPathExcel = "$outputReportDirectory\MinutesPerApp_$(Get-Timestamp).xlsx"
 $reportPathCsv = "$outputReportDirectory\MinutesPerApp_$(Get-Timestamp).csv"
 
-# Create file using New-Item to ensure the parent directory is also created.
 @($reportPathExcel, $reportPathCsv) | ForEach-Object {
     $path = $_
+    # Create file using New-Item to ensure any parent directories are also created.
     New-Item -Path $path -ItemType File -Force | Out-Null
 }
 
@@ -58,6 +42,8 @@ $yAxis = 'Seconds'
 $chartTitle = 'Minutes per App'
 
 $minutesPerAppChart = New-ExcelChartDefinition -XRange $xAxis -YRange $yAxis -Title $chartTitle -NoLegend
-$summaryData | Export-Excel $reportPathExcel -AutoNameRange #-ExcelChartDefinition $minutesPerAppChart #-Show
+$summaryData | Export-Excel $reportPathExcel -AutoNameRange -ExcelChartDefinition $minutesPerAppChart #-Show
 $summaryData | Export-Csv -Path $reportPathCsv -NoTypeInformation 
+
+# TODO: Add additional sheet (or separate file) having data on time by webpage in Edge
 # Export-Excel $reportPathExcel -WorksheetName 'Tab 2' -AutoSize -AutoFilter
